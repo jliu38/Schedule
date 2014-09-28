5f5396659cc0c1157e3b0c9749f8a30a09f2b41d
jliu38 <jliu38@studentmail.nait.ca>
2014-09-28 15:32:06 -0600

    create Schedule.sln

---
+++ Schedule/Entities/EmployeeLocation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule.Entities
+{
+    public class EmployeeLocation
+    {
+        [Key, Column(Order = 1)]
+        public int EmployeeID { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public int LocationID { get; set; }
+        
+
+        //Navigation Properties Should be declared as virtual
+        public virtual Employee Employee { get; set; }
+        public virtual Location Location { get; set; }
+        // prop
+        //then, [Tab][Tab]
+
+    }
+}
